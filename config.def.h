b66c50f9980aa2c91d8c9ccabac7c7dbe060b4ed
wade <sonic_371@yahoo.com.tw>
2026-06-25 21:14:22 +0800

    feat(dwm): add movestack patch for reordering clients
    Add ability to move clients up/down in the stack:
    
    Key Bindings:
    - Shift+Super+J: Move client down in stack
    - Shift+Super+K: Move client up in stack
    
    Implementation (movestack.c):
    - Find next/previous visible non-floating client in stack
    - Swap selected client with target client in client list
    - Preserve client list ordering for all cases (including adjacent clients)
    - Update selmon->clients pointer if selection moves to head
    - Rearrange after swap to reflect new order
    
    This allows dynamic reordering of windows in the tiled layout
    without needing to rearrange via tags or manual window management.

--- config.def.h
+++ config.def.h
@@ -71,6 +71,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -85,6 +86,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = +0.25} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
